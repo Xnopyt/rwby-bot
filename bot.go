7807b86530486e61e2ee60c35b8f18084973c41c
Xnopyt <billy@xnopyt.info>
2019-01-05 21:07:32 +0000

    Added discord init code

--- bot.go
+++ bot.go
@@ -1,4 +1,18 @@
 package main
 
+import "github.com/bwmarrin/discordgo"
+
 func main() {
+	data := loadData()
+	discord, err := discordgo.New("Bot " + data.botToken)
+	if err != nil {
+		panic(err)
+	}
+	ready := make(chan bool)
+	discord.AddHandler(func(s *discordgo.Session, e *discordgo.Ready) { ready <- true })
+	err = discord.Open()
+	if err != nil {
+		panic(err)
+	}
+	<-ready
 }
